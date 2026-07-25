de566c1656b3694ff9bf782305dd03e1eade0b2f
Kadd007 <katlehonthejane@gmail.com>
2026-07-25 05:01:50 +0200

    Add CourseCard component for course details display

--- src/components/CourseCard.tsx
+++ src/components/CourseCard.tsx
@@ -26,3 +26,50 @@ export default function CourseCard({
     </div>
   );
 }
+import Link from "next/link";
+
+
+type CourseProps = {
+slug:string;
+title:string;
+description:string;
+duration:string;
+};
+
+
+export default function CourseCard({
+slug,
+title,
+description,
+duration
+}:CourseProps){
+
+return (
+
+<div className="card">
+
+<h2>
+{title}
+</h2>
+
+<p>
+{description}
+</p>
+
+<span>
+{duration}
+</span>
+
+
+<br/>
+
+<Link href={`/courses/${slug}`}>
+View Course
+</Link>
+
+
+</div>
+
+);
+
+}
