1e3c7bff0c9916b24c56dbabcfd68198a38c5612
Kadd007 <katlehonthejane@gmail.com>
2026-07-25 04:51:42 +0200

    Add team data structure with roles and names

---
+++ src/data/team.ts
@@ -0,0 +1,18 @@
+export const team = [
+
+  {
+    name:"Elly",
+    role:"Founder & Beauty Specialist"
+  },
+
+  {
+    name:"Beauty Team",
+    role:"Professional Stylists"
+  },
+
+  {
+    name:"Academy Trainers",
+    role:"Beauty Educators"
+  }
+
+];
