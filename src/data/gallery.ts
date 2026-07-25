e85b67897766b561c4b0e56f0c980d4a79fcc03e
Kadd007 <katlehonthejane@gmail.com>
2026-07-25 05:04:13 +0200

    Add gallery data for salon and academy categories

---
+++ src/data/gallery.ts
@@ -0,0 +1,27 @@
+export const gallery = [
+
+  {
+    title: "Luxury Hair Styling",
+    category: "Salon",
+    image: "/images/hair-styling.jpg"
+  },
+
+  {
+    title: "Creative Braiding",
+    category: "Salon",
+    image: "/images/braiding.jpg"
+  },
+
+  {
+    title: "Beauty Training",
+    category: "Academy",
+    image: "/images/training.jpg"
+  },
+
+  {
+    title: "Student Creations",
+    category: "Academy",
+    image: "/images/students.jpg"
+  }
+
+];
