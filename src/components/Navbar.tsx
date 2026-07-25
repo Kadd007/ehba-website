86854dbc380ff713d609a58d03cefc00818cc784
Kadd007 <katlehonthejane@gmail.com>
2026-07-25 04:37:40 +0200

    Enhance Navbar with new navigation links
    Updated Navbar component to include additional links for About, Services, Gallery, and Contact.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,52 @@
 import Link from "next/link";
 
-export default function Navbar() {
-  return (
-    <nav className="navbar">
-      <div className="logo">
-        EHBA
-      </div>
 
-      <div className="links">
-        <Link href="/">Home</Link>
-        <Link href="/salon">Salon</Link>
-        <Link href="/academy">Academy</Link>
-        <Link href="/booking">Book Now</Link>
-      </div>
-    </nav>
-  );
+export default function Navbar(){
+
+return (
+
+<nav className="navbar">
+
+<div className="logo">
+EHBA
+</div>
+
+
+<div className="links">
+
+<Link href="/">
+Home
+</Link>
+
+<Link href="/about">
+About
+</Link>
+
+<Link href="/services">
+Services
+</Link>
+
+<Link href="/academy">
+Academy
+</Link>
+
+<Link href="/gallery">
+Gallery
+</Link>
+
+<Link href="/contact">
+Contact
+</Link>
+
+<Link href="/booking">
+Book Now
+</Link>
+
+
+</div>
+
+</nav>
+
+);
+
 }
