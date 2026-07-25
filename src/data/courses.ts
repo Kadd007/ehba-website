1a77177010136931401109b333137024b2d18987
Kadd007 <katlehonthejane@gmail.com>
2026-07-25 04:13:58 +0200

    Add courses data for hair styling and beauty
    Added an array of course objects with titles, descriptions, and durations.

---
+++ src/data/courses.ts
@@ -0,0 +1,20 @@
+export const courses = [
+  {
+    title: "Beginner Hair Styling",
+    description:
+      "Learn the foundations of professional hair styling.",
+    duration: "8 Weeks"
+  },
+  {
+    title: "Advanced Braiding Techniques",
+    description:
+      "Master modern braiding methods and creative designs.",
+    duration: "12 Weeks"
+  },
+  {
+    title: "Beauty Business Skills",
+    description:
+      "Learn how to build and manage a successful beauty business.",
+    duration: "6 Weeks"
+  }
+];
