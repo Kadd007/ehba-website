4e266ecf5d5ab53429b94a96e962134db2e87508
Kadd007 <katlehonthejane@gmail.com>
2026-07-25 04:00:34 +0200

    Create Footer.tsx

---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <p>
+        © {new Date().getFullYear()} Elly's Hair And Beauty Academy.
+        All rights reserved.
+      </p>
+    </footer>
+  );
+}
